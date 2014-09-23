2a8bcc6f0496d3b791250efdf5fb32c67c7fdc2d
travis.day <travis.day@MU-C3-18.mullen.pvt.k12.co.us>
2014-09-23 09:08:37 -0600

    Actual work on board

--- Tic-Tac-Toe/src/Board.java
+++ Tic-Tac-Toe/src/Board.java
@@ -1,5 +1,29 @@
-
 public class Board
 	{
+	static String board[][]= new String[3][3] ;
+	static int valueTable[][]= new int [3][3];
 
+	public static void fillBoardWithValue()
+		{
+		for (int i=0;i<3;i++)
+			{
+			for (int j=0;j<3;i++)
+				{
+				board[i][j]=" ";
+				}
+			}
+		
+		}
+	public static void printBoard()
+		{
+		System.out.println("    A   B   C");
+		System.out.println("  -------------");
+		System.out.println("A | "+board[0][0]+" | "+board[0][1]+" | "+board[0][2]+" | ");
+		System.out.println("  -------------");
+		System.out.println("B | "+board[1][0]+" | "+board[1][1]+" | "+board[1][2]+" | ");
+		System.out.println("  -------------");
+		System.out.println("C | "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" | ");
+		System.out.println("  -------------");
+		}
+	
 	}
